ca05caf775034b0bbac6c0b99aa57c7160fb261e
Naresh Pratista <2141720057@student.polinema.ac.id>
2024-08-03 00:02:19 +0700

    revisi

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,7 +10,9 @@
     pkgs.unzip
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    MOBILE_PATH = "$WORKSPACE_ROOT/mobile";
+  };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -21,13 +23,13 @@
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
         build-flutter = ''
-          cd /home/user/myapp/android
+          cd $MOBILE_PATH/android
 
           ./gradlew \
             --parallel \
             -Pverbose=true \
             -Ptarget-platform=android-x86 \
-            -Ptarget=/home/user/myapp/lib/main.dart \
+            -Ptarget=$MOBILE_PATH/lib/main.dart \
             -Pbase-application-name=android.app.Application \
             -Pdart-defines=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85NzU1MDkwN2I3MGY0ZjNiMzI4YjZjMTYwMGRmMjFmYWMxYTE4ODlhLw== \
             -Pdart-obfuscation=false \
@@ -46,17 +48,20 @@
       };
       
       # To run something each time the workspace is (re)started, use the `onStart` hook
+      onStart = {
+        cd-to-mobile = "cd $MOBILE_PATH";
+      };
     };
     # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
         web = {
-          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT" "--project-path" "$MOBILE_PATH"];
           manager = "flutter";
         };
         android = {
-          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555" "--project-path" "$MOBILE_PATH"];
           manager = "flutter";
         };
       };
