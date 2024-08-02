172b7d502c6d03b984d1f74fd5fa2b5f83c899fc
nareshpratista28 <nareshpratista28@gmail.com>
2024-08-02 17:16:35 +0000

    update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,67 +1,69 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-23.11";
   packages = [
     pkgs.nodePackages.firebase-tools
     pkgs.jdk17
     pkgs.unzip
   ];
-  # Sets environment variables in the workspace
   env = {
-    MOBILE_PATH = "$WORKSPACE_ROOT/mobile";
+    MOBILE_PATH = "./mobile";
   };
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       "Dart-Code.flutter"
       "Dart-Code.dart-code"
     ];
     workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
         build-flutter = ''
-          cd $MOBILE_PATH/android
-
-          ./gradlew \
-            --parallel \
-            -Pverbose=true \
-            -Ptarget-platform=android-x86 \
-            -Ptarget=$MOBILE_PATH/lib/main.dart \
-            -Pbase-application-name=android.app.Application \
-            -Pdart-defines=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85NzU1MDkwN2I3MGY0ZjNiMzI4YjZjMTYwMGRmMjFmYWMxYTE4ODlhLw== \
-            -Pdart-obfuscation=false \
-            -Ptrack-widget-creation=true \
-            -Ptree-shake-icons=false \
-            -Pfilesystem-scheme=org-dartlang-root \
-            assembleDebug
-
-          # TODO: Execute web build in debug mode.
-          # flutter run does this transparently either way
-          # https://github.com/flutter/flutter/issues/96283#issuecomment-1144750411
-          # flutter build web --profile --dart-define=Dart2jsOptimization=O0 
+          if [ -d "$MOBILE_PATH" ]; then
+            cd "$MOBILE_PATH/android"
+            if [ -f "./gradlew" ]; then
+              ./gradlew \
+                --parallel \
+                -Pverbose=true \
+                -Ptarget-platform=android-x86 \
+                -Ptarget=../lib/main.dart \
+                -Pbase-application-name=android.app.Application \
+                -Pdart-defines=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85NzU1MDkwN2I3MGY0ZjNiMzI4YjZjMTYwMGRmMjFmYWMxYTE4ODlhLw== \
+                -Pdart-obfuscation=false \
+                -Ptrack-widget-creation=true \
+                -Ptree-shake-icons=false \
+                -Pfilesystem-scheme=org-dartlang-root \
+                assembleDebug
+            else
+              echo "gradlew file not found in $MOBILE_PATH/android"
+            fi
+          else
+            echo "Mobile directory not found at $MOBILE_PATH"
+          fi
 
           adb -s localhost:5555 wait-for-device
         '';
       };
       
-      # To run something each time the workspace is (re)started, use the `onStart` hook
       onStart = {
-        cd-to-mobile = "cd $MOBILE_PATH";
+        cd-to-mobile = ''
+          if [ -d "$MOBILE_PATH" ]; then
+            cd "$MOBILE_PATH"
+            echo "Changed directory to $MOBILE_PATH"
+          else
+            echo "Mobile directory not found at $MOBILE_PATH"
+          fi
+        '';
       };
     };
-    # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
         web = {
-          command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT" "--project-path" "$MOBILE_PATH"];
+          command = ["sh" "-c" "cd $MOBILE_PATH && flutter run --machine -d web-server --web-hostname 0.0.0.0 --web-port $PORT"];
           manager = "flutter";
         };
         android = {
-          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555" "--project-path" "$MOBILE_PATH"];
+          command = ["sh" "-c" "cd $MOBILE_PATH && flutter run --machine -d android -d localhost:5555"];
           manager = "flutter";
         };
       };
